0355d9880ef80c951d521af893ed88ca64b4a8fe
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-11-03 21:55:07 +0100

    Generador de contraseñas avanzado

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,28 @@ const checkbox3Element = document.getElementById("checkbox3")
 const checkbox4Element = document.getElementById("checkbox4")
 const generatePasswordElement = document.getElementById("generatePassword")
 const rangeElement = document.getElementById("range")
-const passContentLetters = "abcdfghijklmñopqrstuvwxyz"
-const passContentNumbers = "1234567890"
-const passContentSymbols = " .,/*@;:&%()·/$-><= "
+const uppercaseElement = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+const passContentLettersElement = "abcdfghijklmñopqrstuvwxyz"
+const passContentNumbersEelement = "1234567890"
+const passContentSymbolsElement = " .,/*@;:&%()·/$-><= "
 const lengthPassElement = document.getElementById ("lengthPass")
 const passwordElement = document.getElementById("password")
 
 const checkboxInfo = event => {
- if (event.target.checked){
-    generatePasswordElement.disabled = false
- }else{
-    generatePasswordElement.disabled= true
- }
-console.dir(event.target.checked)
-}
+    if(uppercaseElement.checked){
+        generatePasswordElement.disabled= false
+    }else if (passContentLettersElement.checked){
+        generatePasswordElement .disabled= false
+    }else if (passContentNumbersElement.checked){ 
+        generatePasswordElement .disabled = false
+    }else if (passContentSymbolsElement.checked){
+        generatePasswordElement .disabled =false
+    }else {
+        generatePasswordElement.disables =true
+}    
+    }
+
+
 
 let passwordLength = 0
 const lengthPass = (event) =>{
@@ -36,9 +44,27 @@ const generatePass = () => {
     passwordElement.textContent = newPassword
 }
 
+const selectionPassword = ()=> {
+    let totalCharacter = ""
+    if(uppercaseElement.checked){
+        totalCharacter += uppercaseElement
+    }
+    if (passContentLettersElement.checked){
+        totalCharacter += passContentLettersElement
+        }
+    
+    if (passContentNumbersElement.checked){
+        totalCharacter += passContentLettersElement
+    }
+    if(passContentSymbolsElement.checked){
+        totalCharacter += passContentLettersElement
+    }
+
+    return totalCharacter
+}
 
 
-checkbox1Element.addEventListener("change",checkboxInfo)
+generatePasswordElement.addEventListener("change",checkboxInfo)
 checkbox2Element.addEventListener("change",checkboxInfo)
 checkbox3Element.addEventListener("change",checkboxInfo)
 checkbox4Element.addEventListener("change",checkboxInfo)
